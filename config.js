8f1827a8b4d5de47f42be989c19c0a2b1a198ae0
Chasith-Randima <chasith19@gmail.com>
2022-01-03 21:03:35 +0530

    config js changed'

--- config.js
+++ config.js
@@ -3,8 +3,11 @@ import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 
 export const API = publicRuntimeConfig.PRODUCTION
-  ? "https://seoblog.com"
+  ? "https://infinite-savannah-30047.herokuapp.com/api"
   : "http://127.0.0.1:8000/api";
+// export const API = publicRuntimeConfig.PRODUCTION
+//   ? "https://seoblog.com"
+//   : "http://127.0.0.1:8000/api";
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 
 export const DOMAIN = publicRuntimeConfig.PRODUCTION
